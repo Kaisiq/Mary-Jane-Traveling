a1b008dc1644723a26e3a10385afe589ce24b636
kaisiq <dahudohu@gmail.com>
2023-03-17 22:03:10 +0200

    fix remove license

--- countrymap.js
+++ countrymap.js
@@ -7142,10 +7142,10 @@ var simplemaps_countrymap_mapinfo = {
                     "overflow: visible !important; clip-path: none !important; display:inline !important; opacity:1 !important; transform: none !important; visibility: visible !important; z-index: 1 !important; right: 5px !important; bottom:" +
                     e +
                     " !important; z-index: 1 !important; position: absolute !important; filter: opacity(1) !important;"),
-                //     e8.appendChild(tI),
+                    e8.appendChild(tI),
                 //     (tI.innerHTML =
                 //         '<a style="overflow: visible !important; clip-path: none !important; opacity: 1 !important;  filter: opacity(1) !important; transform: none !important; display: block !important; visibility: visible !important; font: 18px Verdana, Arial, Helvetica, sans-serif !important; cursor: pointer !important; font-weight: bold !important; float: right !important; color: #000000 !important; text-decoration: none !important;" href="https://simplemaps.com" title="For evaluation use only.">Simplemaps.com Trial</a>'),
-                // !ea && 503 != tI.innerHTML.length && (e8.innerHTML = "");
+                    !ea && 503 != tI.innerHTML.length && (e8.innerHTML = "");
             }
         }
         function tj() {
