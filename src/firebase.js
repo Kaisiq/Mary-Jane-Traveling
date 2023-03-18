c19d33b08c6645096d1568f5392ac311471aecd1
Teodor Hristov <tedotedo771@gmail.com>
2023-03-18 15:58:56 +0200

    add database url

--- src/firebase.js
+++ src/firebase.js
@@ -8,6 +8,7 @@ const firebaseConfig = {
     authDomain: "maryjanetravelling-e0334.firebaseapp.com",
     projectId: "maryjanetravelling-e0334",
     storageBucket: "maryjanetravelling-e0334.appspot.com",
+    databaseURL: "https://maryjanetravelling-e0334-default-rtdb.europe-west1.firebasedatabase.app/"
     messagingSenderId: "577179775073",
     appId: "1:577179775073:web:1b1c06c5a7f0ae58627a20",
     measurementId: "G-J1GKGQT23L"
