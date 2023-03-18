8e65b525f64b7748f14823e97a1476b565ce423b
Philip3826 <philipgg11@gmail.com>
2023-03-18 02:57:22 +0200

    basic code for the login screen
    закоментирал съм това което брейква кода

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState ,Component} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
+import LoginForm from './login';
+import firebaseConfig from './firebase';
+import firebase from './firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+class placeHolderName extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+        <LoginForm firebase = {firebase}/>
+      </div>
+    );
+  }
+}
+export default placeHolderName;
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
+); 
+// something like this should be for the displaying of the login form
+/*ReactDOM.hydrate(
+  <React.StrictMode>
+    <placeHolderName/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+// or this
+/*root.render(
+  <React.StrictMode>
+    <placeHolderName/>
+  </React.StrictMode>
+);*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/login.js
+++ src/login.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Login from "./Components/Login";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import firebase from './firebase';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <Header />
-        <Login />
-        <Footer/>
-    </React.StrictMode>
-);
+
+function LoginForm(){
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+  
+    const handleEmailChange = (event) =>{
+      setEmail(event.target.value);
+    }
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value);
+    };
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        firebase.auth().signInWithEmailAndPassword(email, password)
+          .then((userCredential) => {
+            
+            firebase.firestore().collection('users').doc(userCredential.user.uid).set({
+              email: userCredential.user.email,
+              displayName: userCredential.user.displayName,
+              photoURL: userCredential.user.photoURL,
+            });
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <button type="submit">Sign In</button>
+      </form>
+    );
+  }
+  export default LoginForm;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
