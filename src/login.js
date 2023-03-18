883d15015c76d2c5dd2acde032eeaa142ccc3081
kaisiq <dahudohu@gmail.com>
2023-03-18 16:59:16 +0200

    add fb and google login buttons

--- src/login.js
+++ src/login.js
@@ -5,6 +5,8 @@ import reportWebVitals from './reportWebVitals';
 import firebase from './firebase';
 import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 import './login.css'
+import fblogo from './media/facebook.webp'
+import googlelogo from './media/google.png'
 
 
 
@@ -76,6 +78,9 @@ function LoginForm({onLogin,onRegister}){
 
     return (
     <div className='login-form'>
+        <div id="fb-root"></div>
+        <script async defer crossOrigin="anonymous"
+                src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v16.0" nonce="rqvBXiYe"></script>
         <div id={"map"} className={'background'}></div>
         <h2>{isRegistering ? 'Регистрация' : 'Влизане'}</h2>
       <form onSubmit={handleSubmit}>
@@ -89,9 +94,18 @@ function LoginForm({onLogin,onRegister}){
           <input type="password" value={password} placeholder={"your password"} onChange={handlePasswordChange} />
           <button onClick={zoomin} type="submit">{isRegistering ? 'Регистриране' : 'Влизане'}</button>
       </form>
+        <div className={"flex"}>
       <button onClick={toggleRegister}>
         {isRegistering ? 'Вече имаш акаунт?' : 'Регистрация'}
       </button>
+            <button>
+                Админ
+            </button>
+        </div>
+        <div className={"flex"}>
+            <button className={"small-img"}><img src={fblogo} alt={"fblogo"} /></button>
+            <button className={"small-img"}><img src={googlelogo} alt={"googlelogo"}/></button>
+        </div>
       </div>
     );
   }
