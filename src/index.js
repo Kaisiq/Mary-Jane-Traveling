94eeeec5b191ffb9be5132f466e19e7057cb9e7b
kaisiq <dahudohu@gmail.com>
2023-03-18 16:32:22 +0200

    add buttons logout and profile

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const isLogged = false;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StartPage/>
+    <Logged />
   </React.StrictMode>
 )
 // export default placeHolderName;
