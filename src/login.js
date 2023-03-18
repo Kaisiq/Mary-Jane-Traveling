1e5296db18115499b60199add866ba21cc97f8e2
Teodor Hristov <tedotedo771@gmail.com>
2023-03-18 21:07:02 +0200

    format

--- src/login.js
+++ src/login.js
@@ -2,37 +2,30 @@ import React, { useState } from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import firebase from './firebase';
-import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword,signInWithPopup,GoogleAuthProvider } from "firebase/auth";
-import { redirect, useParams } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from "react-router-dom";
 import './login.css'
 import fblogo from './media/facebook.webp'
 import googlelogo from './media/google.png'
 
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRegistering, setIsRegistering] = useState(false);
+  const navigate = useNavigate();
+  const provider = new GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
-
-function LoginForm(){
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    const [isRegistering, setIsRegistering] = useState(false);
-    const navigate = useNavigate();
-    const provider = new GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    
-    const handleEmailChange = (event) =>{
-      setEmail(event.target.value);
-    }
-    const handlePasswordChange = (event) => {
-      setPassword(event.target.value);
-    };
-    function toggleRegister(){
-     setIsRegistering(!isRegistering);
-          }
-
-
-  
-
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  function toggleRegister() {
+    setIsRegistering(!isRegistering);
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -44,37 +37,37 @@ function LoginForm(){
           const user = userCredential.user;
           console.log('User registered:', user);
 
-              firebase.auth().signInWithEmailAndPassword(email, password)
-                .then((userLoggedInCredential) => {
-                const user = userLoggedInCredential.user;
-                console.log('User logged in:', user);
-                navigate('/ChooseActivites')
-              })
-              .catch((error) => {
-                console.error('Error logging in user:', error);
-              });
-             
-              })
-              .catch((error) => {
-                console.error('Error registering user:', error);
-              });
-          } else {
-            firebase.auth().signInWithEmailAndPassword(email, password)
-              .then((userCredential) => {
-                // User logged in successfully
-                const user = userCredential.user;
-                console.log('User logged in:', user);
-                setTimeout(function() {navigate('/Logged')}, 500);
-              })
-              .catch((error) => {
-                console.error('Error logging in user:', error);
-              });
-            }
-          }
+          firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userLoggedInCredential) => {
+              const user = userLoggedInCredential.user;
+              console.log('User logged in:', user);
+              navigate('/ChooseActivites')
+            })
+            .catch((error) => {
+              console.error('Error logging in user:', error);
+            });
 
-    const handleGoogleSubmit = async() =>{
-      const auth = getAuth();
-      signInWithPopup(auth, provider)
+        })
+        .catch((error) => {
+          console.error('Error registering user:', error);
+        });
+    } else {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+          // User logged in successfully
+          const user = userCredential.user;
+          console.log('User logged in:', user);
+          setTimeout(function () { navigate('/Logged') }, 500);
+        })
+        .catch((error) => {
+          console.error('Error logging in user:', error);
+        });
+    }
+  }
+
+  const handleGoogleSubmit = async () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -93,13 +86,8 @@ function LoginForm(){
         const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
       });
-    }
-     
-  
+  }
 
-    
-
-  
   const zoomin = () => {
     var myImg = document.getElementById("map");
     var currTransform = myImg.style.transform;
@@ -122,15 +110,15 @@ function LoginForm(){
     } catch (error) {
       console.error(error);
     }
-  
-    
-  if (loading) {
-    alert("Loading...");
-  }
 
-  if (error) {
-    alert("An error occured while connecting to facebook.")
-  };
+
+    if (loading) {
+      alert("Loading...");
+    }
+
+    if (error) {
+      alert("An error occured while connecting to facebook.")
+    };
   };
 
   return (
@@ -151,26 +139,21 @@ function LoginForm(){
         <input type="password" value={password} placeholder={"your password"} onChange={handlePasswordChange} />
         <button onClick={zoomin} type="submit">{isRegistering ? 'Регистриране' : 'Влизане'}</button>
       </form>
-        <div className={"flex"}>
-      <button onClick={toggleRegister}>
-        {isRegistering ? 'Вече имаш акаунт?' : 'Регистрация'}
-      </button>
-            <button>
-                Админ
-            </button>
-        </div>
-        <div className={"flex"}>
-            <button className={"small-img"}><img src={fblogo} alt={"fblogo"} /></button>
-            <button className={"small-img"}><img src={googlelogo} alt={"googlelogo"} onClick={handleGoogleSubmit}/></button>
-        </div>
+      <div className={"flex"}>
+        <button onClick={toggleRegister}>
+          {isRegistering ? 'Вече имаш акаунт?' : 'Регистрация'}
+        </button>
+        <button>
+          Админ
+        </button>
       </div>
+      <div className={"flex"}>
+        <button className={"small-img"}><img src={fblogo} alt={"fblogo"} /></button>
+        <button className={"small-img"}><img src={googlelogo} alt={"googlelogo"} onClick={handleGoogleSubmit} /></button>
+      </div>
+    </div>
   );
 }
 export default LoginForm;
 
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
