d515f49fee2f1503c889408065dd0dba24abf6d6
kaisiq <dahudohu@gmail.com>
2023-03-18 12:49:14 +0200

    fix logged page

--- src/Components/Logged.js
+++ src/Components/Logged.js
@@ -7,53 +7,8 @@ import '../login.css'
 
 
 function Logged(){
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    const [isRegistering, setIsRegistering] = useState(false);
-
-    const handleEmailChange = (event) =>{
-        setEmail(event.target.value);
-    }
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-
-                firebase.firestore().collection('users').doc(userCredential.user.uid).set({
-                    email: userCredential.user.email,
-                    displayName: userCredential.user.displayName,
-                    photoURL: userCredential.user.photoURL,
-                });
-            })
-            .catch((error) => {
-                console.log(error.message);
-            });
-    };
-    function toggleRegister(){
-        setIsRegistering(!isRegistering);
-    }
     return (
-        <div className='login-form'>
-            <div className={'background'}></div>
-            <h2>{isRegistering ? 'Регистрация' : 'Влизане'}</h2>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Email:
-                </label>
-                <input type="email" value={email} placeholder={"your@email.com"} onChange={handleEmailChange} />
-                <label>
-                    Password:
-                </label>
-                <input type="password" value={password} placeholder={"your password"} onChange={handlePasswordChange} />
-                <button type="submit">{isRegistering ? 'Регистриране' : 'Влизане'}</button>
-            </form>
-            <button onClick={toggleRegister}>
-                {isRegistering ? 'Вече имаш акаунт?' : 'Регистрация'}
-            </button>
+        <div className='main-screen'>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='login-box-container'>
-        {isLogged ? <LoginForm/> : <Logged/>}
-    <LoginForm/>
+        {isLogged ? <Logged/> : <LoginForm/>}
     </div>
   </React.StrictMode>
 )
