f3ce88636373525fd7314ed1eb67ac5cb226b175
Philip3826 <philipgg11@gmail.com>
2023-03-19 04:04:36 +0200

    updated removeError sleep time

--- src/login.js
+++ src/login.js
@@ -35,7 +35,7 @@ function LoginForm() {
     var myImg = document.getElementById("map");
     myImg.style.transform = '';
     err.textContent = errorMsg;
-    setTimeout(removeError, 2500);
+    setTimeout(removeError, 2000);
   }
 
   const handleSubmit = (event) => {
